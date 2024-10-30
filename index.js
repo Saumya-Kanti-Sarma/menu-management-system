5c90d9e4698921bc18f656355669cde6657d780e
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2024-10-30 21:49:47 +0530

    added all routes

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from "express";
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import cors from "cors"
+import restaurantRoutes from './routes/restaurant.route.js';
+import menuRoutes from './routes/menu.route.js';
+import customerRoutes from './routes/customers.route.js';
+import ratingsRoutes from './routes/ratings.route.js';
+
 
 dotenv.config() // configuring the environment variables
 const app = express() // settin app
@@ -16,6 +21,15 @@ mongoose.connect(MONGO_URL)
   .catch((err) => console.log(`Error in connection of dataBase: ${err}`))
 
 
+app.use(express.json()) // so we can sent JSON file into databse
+app.use('/restaurant', restaurantRoutes);// restaurant route
+app.use('/restaurant/menu', menuRoutes);// restaurant route
+
+app.use('/customer', customerRoutes);// customer route
+app.use('/customer/ratings', ratingsRoutes);// customer route
+
 //app set-up
 app.listen(PORT, () => { console.log(`server running at http://localhost:${PORT}`) })
 app.get("/", (req, res) => { res.send("hello world") })
+
+
